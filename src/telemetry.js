69e10cfbfb86a465ee11ab2804d0bd5a910fafdf
Jani Seppälä <jani.seppala@jamk.fi>
2026-06-08 14:02:35 +0300

    Telemetry: Change from GA to Matomo

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,30 +1,39 @@
 import Raven from "raven-js";
 import configs from "./utils/configs";
 
-const ga = window.ga;
+let _paq = undefined;
 
 export default function registerTelemetry(trackedPage, trackedTitle) {
   const sentryDsn = configs.SENTRY_DSN;
-  const gaTrackingId = configs.GA_TRACKING_ID;
+
+  if (!_paq) {
+    _paq = window._paq = window._paq || [];
+    _paq.push(["enableLinkTracking"]);
+    (function () {
+      const u = `//analytics.${window.location.origin.split("//")[1]}/`;
+      console.log(`Tracking: initialize Matomo, ${u}`);
+      _paq.push(["setTrackerUrl", u + "matomo.php"]);
+      _paq.push(["setSiteId", "1"]);
+      const d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src = u + "matomo.js";
+      s.parentNode.insertBefore(g, s);
+    })();
+  }
 
   if (sentryDsn) {
     console.log("Tracking: Sentry DSN: " + sentryDsn);
     Raven.config(sentryDsn).install();
   }
 
-  if (ga && gaTrackingId) {
-    console.log("Tracking: Google Analytics ID: " + gaTrackingId);
-
-    ga("create", gaTrackingId, "auto");
-
-    if (trackedPage) {
-      ga("set", "page", trackedPage);
-    }
-
-    if (trackedTitle) {
-      ga("set", "title", trackedTitle);
-    }
-
-    ga("send", "pageview");
+  const email = window.APP.store.state.credentials.email;
+  window._paq.push(["setCustomUrl", trackedPage]);
+  window._paq.push(["setDocumentTitle", trackedTitle]);
+  if (email) {
+    window._paq.push(["setUserId", email]);
   }
+  console.log(`Tracking: Page View: ${trackedPage}, ${trackedTitle}`);
+  window._paq.push(["trackPageView"]);
 }
