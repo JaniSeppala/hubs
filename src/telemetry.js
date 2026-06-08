2460e4e0d498c8b84eaf7a1d2d3a7ce6d25fcd30
Jani Seppälä <jani.seppala@jamk.fi>
2026-06-08 17:04:03 +0300

    Fix Telemetry

--- src/telemetry.js
+++ src/telemetry.js
@@ -6,12 +6,13 @@ let _paq = undefined;
 export default function registerTelemetry(trackedPage, trackedTitle) {
   const sentryDsn = configs.SENTRY_DSN;
 
+  trackedTitle = "Hubs - " + trackedTitle;
+
   if (!_paq) {
     _paq = window._paq = window._paq || [];
-    _paq.push(["enableLinkTracking"]);
     (function () {
       const u = `//analytics.${window.location.origin.split("//")[1]}/`;
-      console.log(`Tracking: initialize Matomo, ${u}`);
+      console.log(`Tracking: Initialize Matomo, ${u}`);
       _paq.push(["setTrackerUrl", u + "matomo.php"]);
       _paq.push(["setSiteId", "1"]);
       const d = document,
@@ -28,12 +29,17 @@ export default function registerTelemetry(trackedPage, trackedTitle) {
     Raven.config(sentryDsn).install();
   }
 
-  const email = window.APP.store.state.credentials.email;
-  window._paq.push(["setCustomUrl", trackedPage]);
-  window._paq.push(["setDocumentTitle", trackedTitle]);
-  if (email) {
-    window._paq.push(["setUserId", email]);
+  //_paq.push(["setCustomUrl", trackedPage]);
+  _paq.push(["setDocumentTitle", trackedTitle]);
+  if (window.APP) {
+    const email = window.APP.store.state.credentials.email;
+    if (email) {
+      _paq.push(["setUserId", email]);
+    }
   }
+
   console.log(`Tracking: Page View: ${trackedPage}, ${trackedTitle}`);
-  window._paq.push(["trackPageView"]);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+  _paq.push(["enableHeartBeatTimer"]);
 }
